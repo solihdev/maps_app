48dc0aee39f2749548c3753e735f3ccb4bb5fbda
Abdumutalibovo <odilbekjuraev07@gmail.com>
2022-12-27 18:59:42 +0500

    some changes

--- lib/ui/splash_screen/splash_screen.dart
+++ lib/ui/splash_screen/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:mapping_app/ui/map_screen/map_screen.dart';
+import 'package:mapping_app/utils/my_images.dart';
 import 'package:mapping_app/view_models/splash_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -16,35 +18,61 @@ class _SplashScreenState extends State<SplashScreen> {
     return ChangeNotifierProvider(
       create: (context) => SplashViewModel(),
       builder: (context, child) {
-        return Scaffold(
-          body: Consumer<SplashViewModel>(
-            builder: (context, viewModel, child) {
-              return Center(
-                child: SizedBox(
-                  width: double.infinity,
-                  height: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text("Splash Screen"),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (viewModel.latLong != null) {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      MapScreen(latLong: viewModel.latLong!),
-                                ));
-                          }
-                        },
-                        child: const Text("Map"),
-                      ),
-                    ],
+        return SafeArea(
+          child: Scaffold(
+            body: Consumer<SplashViewModel>(
+              builder: (context, viewModel, child) {
+                return Center(
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 100,),
+                        const Text(
+                          "Welcome to Google Map",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700, fontSize: 34),
+                        ),
+                        const SizedBox(height: 100,),
+                        SizedBox(
+                            width: double.infinity,
+                            height: 400,
+                            child: Lottie.asset(MyImages.area_map)),
+                        const SizedBox(height: 70,),
+                        InkWell(
+                          child: Container(
+                            width: 150,
+                            height: 40,
+                            decoration: BoxDecoration(
+                                color: Colors.blue.withOpacity(0.4),
+                                borderRadius: BorderRadius.circular(6)),
+                            child: const Center(
+                              child: Text(
+                                "Get Started",
+                                style: TextStyle(
+                                    fontSize: 28, fontWeight: FontWeight.w700),
+                              ),
+                            ),
+                          ),
+                          onTap: () {
+                            if (viewModel.latLong != null) {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        MapScreen(latLong: viewModel.latLong!),
+                                  ));
+                            }
+                          },
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
         );
       },
---
+++ lib/utils/my_images.dart
@@ -0,0 +1,4 @@
+class MyImages{
+  static const lot_map='assets/lotties/lot_map.json';
+  static const area_map='assets/lotties/area_map.json';
+}
